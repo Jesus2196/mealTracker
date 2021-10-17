88cd3ff79f9169d2907f660c6b6635db1302636a
Jesus <Jesuspineda9512@gmail.com>
2021-10-17 10:50:24 -0700

    Index page styling

--- models/meal.js
+++ models/meal.js
@@ -2,17 +2,24 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 const mealSchema = new Schema({
+    date: {
+        type: Date,
+    },
     breakfast: {
         type: String,
+        default: 'N/A',
     },
     lunch: {
         type: String,
+        default: 'N/A',
     },
     dinner: {
         type: String,
+        default: 'N/A',
     },
     snacks: {
         type: String,
+        default: 'N/A',
     }
 });
 
